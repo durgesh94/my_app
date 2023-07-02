38786539fd55b4d798f6f339ee902470a19f72dc
jtambedu <durgesh.tambe@jci.com>
2023-07-02 22:49:21 +0530

    init app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/expenses/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Text('My App'),
+      home: Expenses(),
     ),
   );
 }
