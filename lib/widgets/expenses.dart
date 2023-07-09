f56144e36f15570800ecc113abf89c57d309c92b
jtambedu <durgesh.tambe@jci.com>
2023-07-09 13:15:16 +0530

    added snackbar & undo expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -42,14 +42,40 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
-  void _RemoveExpense(Expense expense) {
+  void _removeExpense(Expense expense) {
+    final idx = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 5),
+        content: const Text('Expense deleted.'),
+        showCloseIcon: true,
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(idx, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found. Start adding some!"),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -63,10 +89,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(children: [
         const Text('The Chart!'),
         Expanded(
-          child: ExpensesList(
-            expenses: _registeredExpenses,
-            onRemoveExpense: _RemoveExpense,
-          ),
+          child: mainContent,
         ),
       ]),
     );
